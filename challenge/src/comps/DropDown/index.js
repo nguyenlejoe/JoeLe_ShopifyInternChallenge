36a0860a107509663abd1ae9e773ce46f78c8ec9
nguyenlejoe <joe_le.1@hotmail.com>
2021-05-06 17:00:14 -0700

    CSS change

--- challenge/src/comps/DropDown/index.js
+++ challenge/src/comps/DropDown/index.js
@@ -38,7 +38,7 @@ box-shadow: 0px 2px 1px rgba(0, 0, 0, 0.05), 0px 0px 1px rgba(0, 0, 0, 0.25);
 border-radius: 8px;
 padding:25px;
 margin-bottom:2%;
-width:200px;
+width:130px;
 height:350px;
 align-items:center;
 
